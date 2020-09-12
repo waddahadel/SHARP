09141b0c42a3a8ba29dae3dfdf9bb227d6956b6f
waddahadel <67363031+waddahadel@users.noreply.github.com>
2020-09-12 23:24:23 +0200

    django project to be deployed

--- Fliper/settings.py
+++ Fliper/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 's1u!!##t1uz!tub(ehz@5#os2-+@gd((48q$1okdzzi7!x_@&+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['fliperreviews.herokuapp.com','127.0.0.1']
 
 
 
