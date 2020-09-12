4bbdd9e67e87af66eeb4a847020dc44e50520925
waddahadel <67363031+waddahadel@users.noreply.github.com>
2020-09-12 23:42:57 +0200

    whitnoise error

--- Fliper/settings.py
+++ Fliper/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -119,16 +120,18 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR='staticfiles')
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR='static')
-# Base url to serve media files
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
 
 MEDIA_URL = '/media/'
 
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR,'static/media')
+]
 # Path where media is stored
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media')
 
 # Application definition
 
